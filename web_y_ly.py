7ac1f35d031981148a74c364a6fa660c073f2222
christon1211-MaiHoaDichSo <christon1211@gmail.com>
2026-03-22 16:20:37 -0500

    Add files via upload

--- web_y_ly.py
+++ web_y_ly.py
@@ -510,7 +510,7 @@ def render_ui_battu_tietkhi(
             width: auto;
             object-fit: contain;
             margin: -55px 0; /* Cắt gọt rìa trong suốt thừa */
-            transform: scale(2.0); /* Phóng to logo lên cho rõ nét */
+            transform: scale(5.0); /* Phóng to logo lên cho rõ nét */
             transform-origin: center center; /* Khóa tâm, chống lệch */
         }}
         .fallback-ct {{ font-size: 12px; font-weight: bold; background: #fff; padding: 3px 12px; border-radius: 12px; border: 2px solid #aaa; color: #555; font-family: Arial, sans-serif; }}
